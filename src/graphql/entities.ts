3d64d33d46609a218aaa3a35faa8b28ed2c89f64
Killian Gemoets <kilgem@live.fr>
2024-06-05 15:19:47 +0200

    fix: build

--- src/graphql/entities.ts
+++ src/graphql/entities.ts
@@ -1,18 +1,18 @@
-import { gql } from "apollo-server";
+// import { gql } from "apollo-server";
 
-export const GET_ENTITIES = gql`
-  query GetEntities {
-    getEntities {
-      id
-      name
-      ... on Contact {
-        email
-        phone
-      }
-      ... on Company {
-        industry
-        contactEmail
-      }
-    }
-  }
-`;
+// export const GET_ENTITIES = gql`
+//   query GetEntities {
+//     getEntities {
+//       id
+//       name
+//       ... on Contact {
+//         email
+//         phone
+//       }
+//       ... on Company {
+//         industry
+//         contactEmail
+//       }
+//     }
+//   }
+// `;
